713e1480d27e4ece72169b2c3e386296fd16863b
maxbendas <56072735+maxbendas@users.noreply.github.com>
2021-01-13 22:20:02 +0200

    3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Navbar from "./components/Navbar";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
 
 function App() {
-  return (
-    <div>Hello world</div>
-  );
+    return (
+        <Router>
+            <Navbar/>
+            <div className="container pt-5">
+                <Switch>
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/about" component={About}/>
+                    <Route path="/profile/:name" exact component={Profile}/>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Link, NavLink} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-dark bg-primary navbar-expand-sm">
+            <div className="navbar-brand">
+                Github Search
+            </div>
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <NavLink exact className="nav-link" to="/">Home</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/about">About</NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
